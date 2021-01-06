98a3d801cff3f03ce83bce510122fb937fd8c325
“lilyXXliu” <eqhylxx@gmail.com>
2021-01-06 04:45:50 -0800

    add predictor

--- example_resnet.py
+++ example_resnet.py
@@ -2,20 +2,13 @@ import sys
 import os
 import math
 import mxnet as mx
-import memonger
 
 import time
 import psutil
 import gc
 
-import numpy as np
-from gluoncv.data import ImageNet
-from mxnet.gluon.data import DataLoader
-from mxnet.gluon.data.vision import transforms
-from mxnet import gluon, nd
-from gluoncv.utils import makedirs, TrainingHistory
-from mxnet import autograd as ag
-from mxnet import profiler, monitor
+from mxnet import profiler
+from util import *
 
 profiler.set_config(profile_all=True,
                     aggregate_stats=True,
@@ -31,56 +24,6 @@ def cpuStats():
     memoryUse = py.memory_info()[0] / 2. ** 30  # memory use in GB...I think
     # print('memory GB:', memoryUse)
     return memoryUse
-
-
-def ConvModule(sym, num_filter, kernel, pad=(0, 0), stride=(1, 1), fix_gamma=True):
-    conv = mx.sym.Convolution(data=sym, kernel=kernel, stride=stride, pad=pad, num_filter=num_filter)
-    bn = mx.sym.BatchNorm(data=conv, fix_gamma=fix_gamma)
-    act = mx.sym.LeakyReLU(data=bn, act_type="leaky") # same memory to our act, less than CuDNN one
-    return act
-
-def ResModule(sym, base_filter, stage, layer, fix_gamma=True):
-    num_f = base_filter * int(math.pow(2, stage))
-    s = 1
-    if stage != 0 and layer == 0:
-        s = 2
-    conv1 = ConvModule(sym, num_f, kernel=(1, 1), pad=(0, 0), stride=(1, 1))
-    conv2 = ConvModule(conv1, num_f, kernel=(3, 3), pad=(1, 1), stride=(s, s))
-    conv3 = ConvModule(conv2, num_f * 4, kernel=(1, 1), pad=(0, 0), stride=(1, 1))
-
-    if layer == 0:
-        sym = ConvModule(sym, num_f * 4, kernel=(1, 1), pad=(0, 0), stride=(s, s))
-
-    sum_sym = sym + conv3
-    # Annotate the critical points that can be saved as inter-stage parameter
-    sym._set_attr(mirror_stage='True')
-    return sum_sym
-
-def get_symbol(layers):
-    """Get a 4-stage residual net, with configurations specified as layers.
-
-    Parameters
-    ----------
-    layers : list of stage configuratrion
-    """
-    assert(len(layers) == 4)
-    base_filter = 64
-    data = mx.sym.Variable(name='data')
-    conv1 = ConvModule(data, base_filter, kernel=(7, 7), pad=(3, 3), stride=(2, 2))
-    mp1 = mx.sym.Pooling(data=conv1, pool_type="max", kernel=(3, 3), stride=(2, 2))
-    sym = mp1
-    for j in range(len(layers)):
-        for i in range(layers[j]):
-            sym = ResModule(sym, base_filter, j, i)
-
-    # for i in range(15):
-    #     sym = ConvModule(sym, base_filter, kernel=(3, 3), pad=(2, 2), stride=(1, 1))
-    #
-    avg = mx.symbol.Pooling(data=sym, kernel=(2, 2), stride=(1, 1), name="global_pool", pool_type='avg')
-    flatten = mx.symbol.Flatten(data=avg, name='flatten')
-    fc1 = mx.symbol.FullyConnected(data=flatten, num_hidden=200, name='fc1')
-    net = mx.symbol.SoftmaxOutput(data=fc1, name='softmax')
-    return net
 
 
 jitter_param = 0.4
@@ -115,25 +58,6 @@ def get_train_iter(dshape):
         pca_noise=lighting_param,
     )
 
-def get_model(dshape, layers, checkpoint=0):
-    net = get_symbol(layers)
-    old_cost = memonger.get_cost(net, data=dshape)
-    print('Old feature map cost=%d MB' % old_cost)
-    if checkpoint > 0:
-      #  net = memonger.search_plan(net, data=dshape)
-        plan_info = {}
-        net = memonger.make_mirror_plan(net, checkpoint, plan_info, data=dshape)
-        print(plan_info)
-        new_cost = memonger.get_cost(net, data=dshape)
-        print('New feature map cost=%d MB' % new_cost)
-        exit()
-    mod = mx.mod.Module(symbol=net,
-                        context=mx.cpu(),
-                        data_names=['data'],
-                        label_names=['softmax_label'])
-
-    return mod
-
 
 
 if __name__ == "__main__":
@@ -142,7 +66,6 @@ if __name__ == "__main__":
     num_threads = str(sys.argv[3])
 
     layers = [3, 24, 36, 3]
-    os.environ["MXNET_CPU_WORKER_NTHREADS"] = "4"
     dshape = (batch_size, 3, 38, 38)
     mod = get_model(dshape, layers=layers, checkpoint=threshold)
 
@@ -191,137 +114,3 @@ if __name__ == "__main__":
         time_per_img = (end - start) * 1.0 / batch_size / repeat_times
         print("batch\tthreshold\tthread number\ttime per image\tmemory (GB)")
         print("%d\t%d\t%s\t%s\t%f" %(batch_size, threshold, os.environ["MXNET_CPU_WORKER_NTHREADS"], time_per_img,  cpuStats()))
-        exit()
-
-#
-#
-# num_cpus = 4
-# ctx = [mx.cpu(i) for i in range(num_cpus)]
-#
-# lr_decay = 0.1
-# # Epochs where learning rate decays
-# lr_decay_epoch = [30, 60, 90, np.inf]
-#
-# # Nesterov accelerated gradient descent
-# optimizer = 'nag'
-# # Set parameters
-# optimizer_params = {'learning_rate': 0.1, 'wd': 0.0001, 'momentum': 0.9}
-#
-# # # Define our trainer for net
-# # trainer = gluon.Trainer(net.collect_params(), optimizer, optimizer_params)
-# #
-# # loss_fn = gluon.loss.SoftmaxCrossEntropyLoss()
-#
-# epochs = 120
-# lr_decay_count = 0
-# log_interval = 50
-#
-# acc_top1 = mx.metric.Accuracy()
-# acc_top5 = mx.metric.TopKAccuracy(5)
-# train_history = TrainingHistory(['training-top1-err', 'training-top5-err',
-#                                  'validation-top1-err', 'validation-top5-err'])
-#
-#
-#
-# def test(ctx, val_data):
-#     acc_top1_val = mx.metric.Accuracy()
-#     acc_top5_val = mx.metric.TopKAccuracy(5)
-#     for i, batch in enumerate(val_data):
-#         data = gluon.utils.split_and_load(batch.data[0], ctx_list=ctx, batch_axis=0)
-#         label = gluon.utils.split_and_load(batch.label[0], ctx_list=ctx, batch_axis=0)
-#         outputs = [net(X) for X in data]
-#         acc_top1_val.update(label, outputs)
-#         acc_top5_val.update(label, outputs)
-#
-#     _, top1 = acc_top1_val.get()
-#     _, top5 = acc_top5_val.get()
-#     return (1 - top1, 1 - top5)
-#
-# for epoch in range(epochs):
-#     tic = time.time()
-#     btic = time.time()
-#     acc_top1.reset()
-#     acc_top5.reset()
-#
-#     # if lr_decay_period == 0 and epoch == lr_decay_epoch[lr_decay_count]:
-#     #     trainer.set_learning_rate(trainer.learning_rate*lr_decay)
-#     #     lr_decay_count += 1
-#
-#     for i, batch in enumerate(train_data):
-#         print(type(batch))
-#         data = gluon.utils.split_and_load(batch.data[0], ctx_list=ctx, batch_axis=0)
-#         print(type(data))
-#         label = gluon.utils.split_and_load(batch.label[0], ctx_list=ctx, batch_axis=0)
-#         # with ag.record():
-#         #     outputs = [net(X) for X in data]
-#         #     loss = [loss_fn(yhat, y) for yhat, y in zip(outputs, label)]
-#         # ag.backward(loss)
-#         # trainer.step(batch_size)
-#         # acc_top1.update(label, outputs)
-#         # acc_top5.update(label, outputs)
-#         # if log_interval and not (i + 1) % log_interval:
-#         #     _, top1 = acc_top1.get()
-#         #     _, top5 = acc_top5.get()
-#         #     err_top1, err_top5 = (1-top1, 1-top5)
-#         #     print('Epoch[%d] Batch [%d]     Speed: %f samples/sec   top1-err=%f     top5-err=%f'%(
-#         #               epoch, i, batch_size*opt.log_interval/(time.time()-btic), err_top1, err_top5))
-#         #     btic = time.time()
-#
-#     # _, top1 = acc_top1.get()
-#     # _, top5 = acc_top5.get()
-#     # err_top1, err_top5 = (1-top1, 1-top5)
-#     #
-#     # err_top1_val, err_top5_val = test(ctx, val_data)
-#     # train_history.update([err_top1, err_top5, err_top1_val, err_top5_val])
-#     #
-#     # print('[Epoch %d] training: err-top1=%f err-top5=%f'%(epoch, err_top1, err_top5))
-#     # print('[Epoch %d] time cost: %f'%(epoch, time.time()-tic))
-#     # print('[Epoch %d] validation: err-top1=%f err-top5=%f'%(epoch, err_top1_val, err_top5_val))
-#
-# # train_history.plot(['training-top1-err', 'validation-top1-err'])
-
-
-# output_total_size = 0
-# weight_total_size = 0
-# gradient_total_size = 0
-# def cal_output(x):
-#     print("I am in output-----")
-#     global output_total_size
-#     output_total_size += x.size * 4
-#     return mx.nd.array([x.size * 4])
-#
-# def cal_weight(x):
-#     global weight_total_size
-#     weight_total_size += x.size * 4
-#     return mx.nd.array([x.size * 4])
-#
-# def cal_gradient(x):
-#     global gradient_total_size
-#     gradient_total_size += x.size * 4
-#     return mx.nd.array([x.size * 4])
-
-
-#output_mon = monitor.Monitor(1, stat_func=cal_output, pattern='.*output.*', sort=True)
-#weight_mon = monitor.Monitor(1, stat_func=cal_weight, pattern='.*', sort=True)
-#gradient_mon = monitor.Monitor(1, stat_func=cal_gradient, pattern='.*backward.*', sort=True)
-
-#mod.install_monitor(output_mon)
-#mod.install_monitor(weight_mon)
-#mod.install_monitor(gradient_mon)
-
-# val_data = mx.io.ImageRecordIter(
-#     path_imgrec         = './tiny-imagenet_test.rec',
-#     path_imgidx         = './tiny-imagenet_test.idx',
-#     preprocess_threads  = 32,
-#     shuffle             = False,
-#     batch_size          = 256,
-#
-#     resize              = 256,
-#     data_shape          = (3, 64, 64),
-#     mean_r              = mean_rgb[0],
-#     mean_g              = mean_rgb[1],
-#     mean_b              = mean_rgb[2],
-#     std_r               = std_rgb[0],
-#     std_g               = std_rgb[1],
-#     std_b               = std_rgb[2],
-# )
---
+++ predictor/prepare.py
@@ -0,0 +1,206 @@
+import random
+import time
+
+import mxnet as mx
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+batches = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+heights = [16, 32, 64, 128, 256]
+widths = [16, 32, 64, 128, 256]
+kernels = [1, 3, 5, 7, 9, 11, 13, 15, 17]
+strides = [1, 2, 3, 5, 7]
+pads = [1, 2, 3]
+num_filters = [12, 24, 36, 48, 96, 128, 256]
+layouts = [None, 'NCDHW', 'NCHW', 'NCW', 'NDHWC', 'NHWC']
+
+# batches = [1]
+# heights = [3]
+# widths = [3]
+# kernels = [1]
+# strides = [1]
+# pads = [0]
+# num_filters = [1]
+DEBUG = False
+
+repeat_times = 100
+if mx.context.num_gpus():
+    device = mx.gpu()
+else:
+    device = mx.cpu()
+metric = mx.metric.Loss()
+
+random.seed(0)
+
+def SGD(key, weight, grad, grad_norm, lr=0.00001):
+    # key is key for weight, we can customize update rule
+    # weight is weight array
+    # grad is grad array
+    # lr is learning rate
+    # grad_norm is scalar to norm gradient, usually it is batch_size
+    norm = 1.0 / grad_norm
+    # here we can bias' learning rate 2 times larger than weight
+    if "weight" in key or "gamma" in key:
+        weight[:] -= lr * (grad * norm)
+    elif "bias" in key or "beta" in key:
+        weight[:] -= 2.0 * lr * (grad * norm)
+    else:
+        pass
+
+def generate_training_data(num_trails, file_name, name="conv2d"):
+    for i in range(num_trails):
+        params = {}
+        if name == "conv2d":
+        # 1. generate the operator
+            # conv2d
+            channel = 1
+            batch_size = batches[random.randint(0, len(batches) - 1)]
+            height = heights[random.randint(0, len(heights) - 1)]
+            width = widths[random.randint(0, len(widths) - 1)]
+            kernel_value = min(height, width)
+            while kernel_value >= min(height, width):
+                kernel_value = kernels[random.randint(0, len(kernels) - 1)]
+            kernel = (kernel_value, kernel_value)
+            stride_value = strides[random.randint(0, len(strides) - 1)]
+            stride = (stride_value, stride_value)
+            pad_value = pads[random.randint(0, len(pads) - 1)]
+            pad = (pad_value, pad_value)
+            num_filter = num_filters[random.randint(0, len(num_filters) - 1)]
+            layout = layouts[random.randint(0, len(layouts) - 1)]
+
+            params['batch_size'] = batch_size
+            params['height'] = height
+            params['width'] = width
+            params['kernel_value'] = kernel_value
+            params['stride_value'] = stride_value
+            params['pad_value'] = pad_value
+            params['num_filter'] = num_filter
+            print(params)
+
+            data = mx.symbol.stop_gradient(mx.symbol.Variable('data'))
+            conv = mx.sym.Convolution(data=data,
+                                      kernel=kernel,
+                                      stride=stride,
+                                      pad=pad,
+                                      num_filter=num_filter,
+                                      name=name)
+            label = mx.sym.Variable('label')
+            loss = mx.symbol.MakeLoss(mx.sym.square(label - conv))
+            pred_loss = mx.sym.Group([mx.sym.BlockGrad(conv), loss])
+
+            executor = pred_loss.simple_bind(ctx=device, data=(batch_size, channel, height, width), grad_req='write')
+            args = executor.arg_dict
+            grads = executor.grad_dict
+            aux_states = executor.aux_dict
+            outputs = dict(zip(conv.list_outputs(), executor.outputs))
+
+            # print("args %s" % args.keys())
+            # print("grads %s" % grads.keys())
+            # print("aux_states %s" % aux_states.keys())
+            # print("outputs %s" % outputs.keys())
+            # print("-" * 20)
+            args[name + '_weight'][:] = mx.random.uniform(-1, 1, args[name + '_weight'].shape)
+            args[name + '_bias'][:] = mx.random.uniform(-1, 1, args[name + '_bias'].shape)
+
+            inputs = []
+            labels = []
+            for _ in range(repeat_times):
+                data = mx.nd.random.uniform(0, 1,
+                                                shape=(batch_size, channel, height, width),
+                                                ctx=device, dtype='float32')
+                inputs.append(data)
+                args["data"][:] = data
+                executor.forward(is_train=False)
+                labels.append(executor.outputs[0])
+
+            args[name + '_weight'][:] = mx.random.uniform(-1, 1, args[name + '_weight'].shape)
+            args[name + '_bias'][:] = mx.random.uniform(-1, 1, args[name + '_bias'].shape)
+
+            start = time.time()
+            # 2. run the operator multiple times, get the training time
+            keys = conv.list_arguments()
+            ex = pred_loss.simple_bind(ctx=device, data=(batch_size, channel, height, width))
+            for i in range(repeat_times):
+                ex.forward(is_train=True, data=inputs[i], label=labels[i])
+                ex.backward()
+                for key in keys:
+                    SGD(key, ex.arg_dict[key], ex.grad_dict[key], grad_norm=batch_size)
+
+                if DEBUG:
+                    loss = (ex.outputs[0] - labels[i]).asnumpy()
+                    print("Loss", i, loss.sum())
+
+            mx.nd.waitall()
+            end = time.time()
+            print("Exec time: %f" % (end - start))
+
+            # 3. record to file, param1, param2, param3..., training time
+            with open(file_name, "w") as f:
+                for key in params.keys():
+                    f.write(key + ",")
+                f.write("train_time\n")
+                for key in params.keys():
+                    f.write(str(params[key]) + ",")
+                f.write(str(end - start))
+
+        elif type == "pooling":
+            pass
+            # pooling
+        elif type == "relu":
+            pass
+            # relu
+        # fully connected
+
+
+def extract_features(file_name):
+    x = []
+    y = []
+    feature_names = []
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if i % 2 == 0:
+                feature_names = line.split(',')
+            else:
+                x.append([float(x) for x in line.split(',')[:-1]])
+                y.append([float(line.split(',')[-1])])
+
+    return feature_names, x, y
+
+def split(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=0.33,
+                                                        random_state=42)
+    return X_train, X_test, y_train, y_test
+
+
+def train(x, y, name="linear"):
+    model = None
+    if name == "svm":
+        model = svm.SVC(kernel='linear')
+        model.fit(x, y)
+    print("Train accuracy :", r2_score(y, model.predict(x)))
+    return model
+
+
+def test(x, y, model):
+    predict_y = model.predict(x)
+    acc = r2_score(y, predict_y)
+    print("Test accuracy :", acc)
+
+if __name__ == "__main__":
+    # layers = [3, 24, 36, 3]
+    # batch_size = 128
+    # dshape = (batch_size, 3, 38, 38)
+    # mod = get_model(dshape, layers=layers, checkpoint=0)
+    file_name = "conv2d_feature"
+    num_trails = 100
+    generate_training_data(num_trails, file_name)
+    feature_names, x, y = extract_features(file_name)
+    train_x, train_y, test_x, test_y = split(x, y)
+    model = train(train_x, train_y)
+    # test(test_x, test_y, model)
+
+
+
