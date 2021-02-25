d2ef4393bc793b90fc4d632c861a8e786a1fe233
LiuXiaoxuanPKU <lilyliupku@gmail.com>
2021-02-25 00:15:19 -0800

    add relu

--- predictor/prepare.py
+++ predictor/prepare.py
@@ -54,6 +54,7 @@ random.seed(42)
 C_conv = 1 / 300.0 * 1.35
 C_pool = 2
 C_bn = 1 / 2.5 * 0.7
+C_relu = 1
 
 mem_bandwidth = 4.8 * pow(10, 9)
 cpu_freq = 1.8 * pow(10, 9)
@@ -74,7 +75,6 @@ def cost_model_predict(name, data):
         backward_t = forward_t * 2
         return forward_t, backward_t
     elif name == "pool":
-        print("+++++", data)
         output_w = int((data['W'] + 2 * data['pad'][0] - data['kernel'][0] + 1) / data['stride'][0])
         output_h = int((data['H'] + 2 * data['pad'][1] - data['kernel'][1] + 1) / data['stride'][1])
         write_num = output_w * output_h * data['N']
@@ -91,7 +91,12 @@ def cost_model_predict(name, data):
         backward_t = forward_t
         return forward_t, backward_t
     elif name == "relu":
-        return 0, 0
+        write_num = data['N'] * data['IC'] * data['H'] * data['W']
+        read_num = write_num
+        flops =read_num
+        forward_t = C_bn * cost_mode(read_num, write_num, flops)
+        backward_t = forward_t
+        return forward_t, backward_t
     else:
         print ("Unsupport operator type %s, return 0" % name)
         return 0, 0
@@ -160,250 +165,10 @@ def generate_params():
     return all_params
 
 
-def get_train_time(category, executor, operator, inputs, labels, all_params):
-    keys = operator.list_arguments()
-    if category == "forward+backward":
-        start = time.time()
-        for i in range(repeat_times):
-            out = executor.forward(is_train=True, data=inputs[i], label=labels[i])
-            if DEBUG:
-                # print(out[0])
-                loss = (out[0] - labels[i]).asnumpy()
-                print("Loss", i, loss.sum())
-
-            executor.backward()
-            for key in keys:
-                SGD(key, executor.arg_dict[key], executor.grad_dict[key], grad_norm=all_params['batch_size'])
-
-        mx.nd.waitall()
-        end = time.time()
-
-    elif category == "forward":
-        start = time.time()
-        for i in range(repeat_times):
-            out = executor.forward(is_train=True, data=inputs[i], label=labels[i])
-        mx.nd.waitall()
-        end = time.time()
-
-    elif category == "backward":
-        start = time.time()
-        for i in range(repeat_times):
-            executor.backward()
-            for key in keys:
-                SGD(key, executor.arg_dict[key], executor.grad_dict[key], grad_norm=all_params['batch_size'])
-        mx.nd.waitall()
-        end = time.time()
-
-    print("Exec time: %f" % (end - start))
-    return end - start
-
-
-def generate_training_data(num_trails, file_name, category, name, cpu_num):
-    print(name)
-    for i in range(num_trails):
-        params = {}
-        data = mx.symbol.stop_gradient(mx.symbol.Variable('data'))
-
-        all_params = generate_params()
-        params['batch_size'] = all_params['batch_size']
-        params['height'] = all_params['height'] if name != "fc" else all_params['batch_size']
-        params['width'] = all_params['width'] if name != "fc" else all_params['height'] * all_params['width']
-        params['cpu_num'] = cpu_num
-
-        dshape = (all_params['batch_size'],
-                  all_params['channel'],
-                  all_params['height'],
-                  all_params['width'])
-        dshape = dshape if name != "fc" else (dshape[0], dshape[2] * dshape[3])
-
-        if name == "conv":
-            # 1. generate the operator
-            # conv2d
-            params['kernel_value'] = all_params['kernel_value']
-            params['stride_value'] = all_params['stride_value']
-            params['pad_value'] = all_params['pad_value']
-            params['num_filter'] = all_params['num_filter']
-            operator = mx.sym.Convolution(data=data,
-                                          kernel=(all_params['kernel_value'], all_params['kernel_value']),
-                                          stride=(all_params['stride_value'], all_params['stride_value']),
-                                          pad=(all_params['pad_value'], all_params['pad_value']),
-                                          num_filter=all_params['num_filter'],
-                                          name=name)
-
-        elif name == "bn":
-            operator = mx.symbol.BatchNorm(data, name='bn')
-
-        elif name == "pool":
-            params['kernel_value'] = all_params['kernel_value']
-            params['stride_value'] = all_params['stride_value']
-            operator = mx.symbol.Pooling(data=data,
-                                         kernel=(all_params['kernel_value'], all_params['kernel_value']),
-                                         stride=(all_params['stride_value'], all_params['stride_value']),
-                                         pool_type=all_params['pool_type'])
-
-        elif name == "relu":
-            raise NotImplemented
-        elif name == "fc":
-            params['hidden_num'] = all_params['hidden_num']
-            operator = mx.symbol.FullyConnected(data=data,
-                                                num_hidden=all_params['hidden_num'],
-                                                name='fc')
-
-        # Get executor
-        executor = get_executor(operator, dshape)
-
-
-        args = executor.arg_dict
-        # print(args.keys())
-
-        # 1. Generate inputs, outputs
-        inputs = []
-        labels = []
-        if name + '_weight' in args.keys():
-            args[name + '_weight'][:] = mx.random.uniform(-1, 1, args[name + '_weight'].shape)
-        if name + '_bias' in args.keys():
-            args[name + '_bias'][:] = mx.random.uniform(-1, 1, args[name + '_bias'].shape)
-        if name + '_gamma' in args.keys():
-            args[name + '_gamma'][:] = mx.random.uniform(-1, 1, args[name + '_gamma'].shape)
-        if name + '_beta' in args.keys():
-            args[name + '_beta'][:] = mx.random.uniform(-1, 1, args[name + '_beta'].shape)
-
-        for _ in range(repeat_times):
-            data = mx.nd.random.uniform(0, 1,
-                                        shape=dshape,
-                                        ctx=device, dtype='float32')
-            inputs.append(data)
-            args["data"][:] = data
-            executor.forward(is_train=False)
-            labels.append(executor.outputs[0].copy())
-
-        if name + '_weight' in args.keys():
-            args[name + '_weight'][:] = mx.random.uniform(-1, 1, args[name + '_weight'].shape)
-        if name + '_bias' in args.keys():
-            args[name + '_bias'][:] = mx.random.uniform(-1, 1, args[name + '_bias'].shape)
-        if name + '_gamma' in args.keys():
-            args[name + '_gamma'][:] = mx.random.uniform(-1, 1, args[name + '_gamma'].shape)
-        if name + '_beta' in args.keys():
-            args[name + '_beta'][:] = mx.random.uniform(-1, 1, args[name + '_beta'].shape)
-
-        # 2. run the operator multiple times, get the training time
-        train_time = get_train_time(category, executor, operator, inputs, labels, all_params)
-
-        # 3. record to file, param1, param2, param3..., training time
-        with open(file_name, "a") as f:
-            for key in params.keys():
-                f.write(key + ",")
-            f.write("train_time\n")
-            for key in params.keys():
-                f.write(str(params[key]) + ",")
-            f.write(str(train_time) + "\n")
-
-
-def extract_features(file_name):
-    x = []
-    y = []
-    feature_names = []
-    with open(file_name, "r") as f:
-        lines = f.readlines()
-        for i, line in enumerate(lines):
-            if i % 2 == 0:
-                feature_names = line.split(',')
-            else:
-                x.append([float(x) for x in line.split(',')[:-1]])
-                y.append([float(line.split(',')[-1])])
-
-    return feature_names, x, y
-
-
-def train(x, y, name="poly"):
-    model = None
-    print(x[0])
-    print(y[0])
-    if name == "svm":
-        model = svm.SVR()
-    elif name == 'linear':
-        model = LinearRegression()
-    elif name == 'poly':
-        model = Pipeline([
-            ("poly", PolynomialFeatures(degree=4)),
-            ("std_scaler", StandardScaler()),
-            ("lin_reg", LinearRegression())
-        ])
-    elif name == "fc":
-        pass
-    model.fit(x, y)
-    print("Train accuracy :", model.score(x, y))
-    return model
-
-
-def test(x, y, model):
-    acc = model.score(x, y)
-    print("Test accuracy :", acc)
-
-def random_split(x, y):
-    train_x, test_x, train_y, test_y = train_test_split(x, y,
-                                                        test_size=0.33,
-                                                        random_state=42)
-    return train_x, train_y, test_x, test_y
-
-def order_split(x, y, feature_names):
-    test_per = 0.33
-    data = np.array([l + y[i] for i,l in enumerate(x)])
-    print(data.shape, feature_names)
-    df = pd.DataFrame(data, columns=feature_names)
-    sort_feature = feature_names[1]
-    print("Sort by feature values ", sort_feature )
-    df = df.sort_values(by=sort_feature)
-    train_data = df.to_numpy()[:int(df.shape[0]*(1-test_per))]
-    test_data = df.to_numpy()[int(df.shape[0]*(1-test_per)):]
-    print(test_data[-1])
-    train_x = train_data[:, :-1]
-    print (train_x[:5])
-    train_y = train_data[:, -1]
-    print (train_y[:5])
-    test_x = test_data[:, :-1]
-    print (test_x[:5])
-    test_y = test_data[:, -1]
-    print (test_y[:5])
-    return train_x, train_y, test_x, test_y
-
-def train_test(filename):
-    feature_names, x, y = extract_features(filename)
-    if SPLIT == "random":
-        train_x, train_y, test_x, test_y = random_split(x, y)
-    elif SPLIT == "order":
-        train_x, train_y, test_x, test_y = order_split(x, y, feature_names)
-    else:
-        print("[Error] Unknown split type ", SPLIT)
-        exit()
-    model = train(train_x, train_y)
-    test(test_x, test_y, model)
-    # TODO: feature_names
-    return model
-
-def get_trained_model(num_trails, opt, filename, cpu_num):
-    print("Fit model for operator " + opt + "-" * 20)
-    forward_filename = filename + "_forward"
-    backward_filename = filename + "_backward"
-    print(forward_filename)
-    if not os.path.exists(forward_filename):
-        generate_training_data(num_trails, forward_filename, "forward", opt, cpu_num)
-
-    if not os.path.exists(backward_filename):
-        generate_training_data(num_trails, backward_filename, "backward", opt, cpu_num)
-
-    # forward
-    forward_model = train_test(forward_filename)
-    backward_model = train_test(backward_filename)
-
-    # backward
-    return {"forward" : forward_model, "backward" : backward_model}
-
-
 def copy_symbol(name, attrs):
     op = None
     data = mx.symbol.Variable('data')
-    print(name)
+    # print(name, attrs)
     if name.find("conv") != -1:
         op = mx.sym.Convolution(data=data,
                                 kernel=make_tuple(attrs['kernel']),
@@ -420,7 +185,7 @@ def copy_symbol(name, attrs):
                             stride=make_tuple(attrs['stride']))
     elif name.find("fc") != -1:
         op = mx.symbol.FullyConnected(data=data, num_hidden=int(attrs['num_hidden']))
-    elif name.find("relu") != -1:
+    elif name.find("relu") != -1 or name.find("activation") != -1:
         op = mx.symbol.relu(data=data)
     elif name.find("flatten") != -1:
         op = mx.symbol.Flatten(data=data, name='flatten')
@@ -430,7 +195,7 @@ def copy_symbol(name, attrs):
 def generate_x(mod, dshape, num_core):
     sym = mod.symbol
     attrs = sym.list_attr()
-    print(sym.name)
+    # print(sym.name)
     name = sym.name
     data = {}
 
@@ -472,7 +237,6 @@ def generate_x(mod, dshape, num_core):
         return [[batch_size, height, width, cpu_num]], data, name
 
     elif name.find("pool") != -1:
-        print(attrs)
         # batch_size,height,width,kernel_value,stride_value
         kernel = make_tuple(attrs['kernel'])
         stride = make_tuple(attrs['stride'])
@@ -496,12 +260,19 @@ def generate_x(mod, dshape, num_core):
         hidden_num = int(attrs['num_hidden'])
         name = "dense"
         return [[batch_size, height, width, cpu_num, hidden_num]], data, name
+    elif name.find("relu") != -1:
+        data['N'] = batch_size
+        data['IC'] = dshape[1]
+        data['H'] = height
+        data['W'] = width
+        name = "relu"
+        return None, data, name
     else:
-        print("[Error] Unsupport type ", name)
+        print("[Error]" , name)
         return None, None, name
 
 def get_opt_name(layer_name):
-    opts = ['conv', 'pool', 'batchnorm', 'dense', 'bn']
+    opts = ['conv', 'pool', 'batchnorm', 'dense', 'bn', 'relu', 'activation']
     for opt in opts:
         if layer_name.find(opt) != -1:
             return opt
@@ -512,6 +283,8 @@ def predict_network(mod, models, org_dshape, num_core):
     total_train_time = 0
     total_conv_time = 0
     total_bn_time = 0
+    total_pool_time = 0
+    total_act_time = 0
 
     dshape = org_dshape
     from collections import namedtuple
@@ -520,9 +293,8 @@ def predict_network(mod, models, org_dshape, num_core):
     all_layers = sym.get_internals()
     for layer_name in all_layers.list_outputs():
         if layer_name.endswith("output"):
-            # print(layer_name)
 
-#            print(all_layers[layer_name].list_attr())
+            print(all_layers[layer_name].list_attr())
             operator_sym = copy_symbol(layer_name, all_layers[layer_name].list_attr())
 
             if operator_sym is None:
@@ -543,14 +315,19 @@ def predict_network(mod, models, org_dshape, num_core):
                 # plus layer is not counted
                 if opt_name:
                     x, data, name = generate_x(operator_mod, dshape, num_core)
-                    print(data)
+                    # print(data)
                     forward_t, backward_t = cost_model_predict(name, data)
                     if name == "batchnorm":
                         total_bn_time += forward_t
                     if name == "conv":
                         total_conv_time += forward_t
                         global conv_cnt
-                        conv_cnt += 1 
+                        conv_cnt += 1
+                    if name == "pool":
+                        total_pool_time += forward_t
+                    if name == "relu":
+                        total_act_time += forward_t
+
 
                     # x = generate_x(operator_mod, dshape, num_core)
                     # print(layer_name, x)
@@ -566,6 +343,8 @@ def predict_network(mod, models, org_dshape, num_core):
 
     print("Predict forward conv ", total_conv_time)
     print("Predict forward bn ", total_bn_time)
+    print("Predict forward pool", total_pool_time)
+    print("Predict forward relu", total_act_time)
 
     return total_train_time
 
@@ -576,40 +355,6 @@ if __name__ == "__main__":
 
     num_trails = 1000
     cpu_num = int(sys.argv[1])
-
-    # conv_filename = "conv"
-    # generate_training_data(num_trails, conv_filename + "_forward", "forward", "conv", cpu_num)
-    # generate_training_data(num_trails, conv_filename + "_backward", "backward", "conv", cpu_num)
-    #
-    # pooling_filename = "pooling"
-    # generate_training_data(num_trails, pooling_filename + "_forward", "forward", "pooling", cpu_num)
-    # generate_training_data(num_trails, pooling_filename + "_backward", "backward", "pooling", cpu_num)
-    #
-    # bn_filename = "bn"
-    # generate_training_data(num_trails, bn_filename + "_forward", "forward", "bn", cpu_num)
-    # generate_training_data(num_trails, bn_filename + "_backward", "backward", "bn", cpu_num)
-    #
-    # fc_filename = "fc"
-    # generate_training_data(num_trails, fc_filename + "_forward", "forward", "fc", cpu_num)
-    # generate_training_data(num_trails, fc_filename + "_backward", "backward", "fc", cpu_num)
-    #
-
-    # dir = "./"
-    # pool_model = get_trained_model(num_trails, "pooling", dir + "pooling", cpu_num)
-    # exit()
-    # conv_model= get_trained_model(num_trails,  "conv", dir + "conv", cpu_num)
-    # fc_model = get_trained_model(num_trails, "fc", dir + "fc", cpu_num)
-    # bn_model = get_trained_model(num_trails, "bn", dir + "bn", cpu_num)
-    # operator_models = {
-    #     "conv" : conv_model,
-    #     "pooling" : pool_model,
-    #     "dense" : fc_model,
-    #     "batchnorm" : bn_model,
-    # }
-    #
-    # print(conv_model['forward'].predict([[256, 64, 64, 8, 5, 1, 1, 256]]))
-    # print('---------------------------')
-    # exit()
 
     mod = get_model(dshape, 0, "res50")
 
